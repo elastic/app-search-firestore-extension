0345378f8883775ac4a954ae8bcdbd18ddd13673
Jason Stoltzfus <jastoltz24@gmail.com>
2021-08-30 14:38:34 -0400

    Added connectivity to App Search

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,10 +1,10 @@
-This project was created using document https://firebase.google.com/docs/extensions/alpha/overview-build-extensions. To view the documentation, you're google account must be whitelisted by Google.
+This project was created using document https://firebase.google.com/docs/extensions/alpha/overview-build-extensions. To view the documentation, your google account must be whitelisted by Google.
 
 This project includes a local firebase project created with the `firebase init` command in order to provide an Emulated firebase environment in which develop the plugin. You can read more about that here: https://firebase.google.com/docs/emulator-suite/connect_and_prototype
 
 ## Set up the firebase CLI
 
-This project uses the firebase CLI, you should install and log into the CLI first. 
+This project uses the firebase CLI, you should install and log into the CLI first.
 
 You'll use the CLI directly to run and publish this project.
 
@@ -14,6 +14,7 @@ firebase login
 ```
 
 You'll also need to unlock the extension development tools:
+
 ```
 firebase --open-sesame extdev
 # Run the following to see what this unlocked for you: firebase --help | grep ext:dev
@@ -32,12 +33,24 @@ The best way to develop an extension is using a local environment, not an actual
 
 This project contains all of the tooling to run an emulated environment already, see the documentation [here](https://firebase.google.com/docs/emulator-suite) to read more.
 
-NOTE: Local projects are set up to mirror *real* projects in the cloud. This project is configured to mirror a project in the cloud called `pokemon-de597`.
+NOTE: Local projects are set up to mirror _real_ projects in the cloud. This project is configured to mirror a project in the cloud called `pokemon-de597`.
 
 ```shell
 cd functions
-npm run build # Typescript must be compiled before we can run them
+npm run build -- -w # Typescript must be compiled before we can run them, and we'll watch (-w) it for changes so we can recompile
 firebase emulators:start
 ```
 
 Navigate to http://localhost:4000/
+
+## Set up connection to App Search
+
+TODO: This section can be removed once the extension is configurable
+
+1. Set up and run App Search locally.
+2. Create a new empty engine called 'pokemon'
+3. In functions/index.ts, change the 'appSearchAPIKey' configuration to use
+
+## Set up a test collection locally
+
+In the Firestore emulator, create a collection called "pokemon" and add a document to it. That document should be syned to your App Search instance.
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,61 @@
 import * as functions from "firebase-functions";
+//@ts-ignore There are no type definitions for this library
+import * as AppSearchClient from "@elastic/app-search-node";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+// TODO This will eventually become configuration from the extension
+// TODO Make this an extension
+const config = {
+  indexName: "pokemon",
+  collectionName: "pokemon",
+  appSearchAPIKey: "private-79iadc5dzd3qxgfgd9w9ryc7",
+  appSearchEngineName: "pokemon",
+  appSearchLocation: "http://localhost:3002",
+};
+
+const client = new AppSearchClient(
+  undefined,
+  config.appSearchAPIKey,
+  () => `${config.appSearchLocation}/api/as/v1/`
+);
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 //
+// TODO This could be come the search function evenually
+// What of search should be supported? Do we support filtering? Can users do a full search including faceting and filter? That would let them use search-ui
+// However, that would mean they'd need to add those fields to the index, and I'm not sure if we'd want to do that or not? Perhaps it's better just to support
+// queries on a single field
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+  functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
+
+// TODO handle nested fields
+// TODO handle only index specified fields
+// TODO index data with correct types? ... that depends on if we want filtering...
+exports.shipToElastic = functions.firestore
+  .document(`/${config.collectionName}/{documentId}`)
+  .onWrite(async (change) => {
+    if (change.before.exists === false) {
+      // TODO Consider log levels
+      console.log(`Creating document: ${change.after.id}`);
+      client.indexDocuments(config.appSearchEngineName, [
+        {
+          id: change.after.id,
+          ...change.after.data(),
+        },
+      ]);
+    } else if (change.after.exists === false) {
+      console.log(`Deleting document: ${change.before.id}`);
+      client.destroyDocuments(config.appSearchEngineName, [change.before.id]);
+    } else {
+      console.log(`Updating document: ${change.after.id}`);
+      client.indexDocuments(config.appSearchEngineName, [
+        {
+          id: change.after.id,
+          ...change.after.data(),
+        },
+      ]);
+    }
+    return change.after;
+  });
