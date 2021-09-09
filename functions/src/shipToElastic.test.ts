f3213e75a7fc84d2e82a8d2f24ba2cec995ca7c6
Jason Stoltzfus <jastoltz24@gmail.com>
2021-09-09 12:11:28 -0400

    Added tests for existing behavior

--- functions/src/shipToElastic.test.ts
+++ functions/src/shipToElastic.test.ts
@@ -1,5 +1,99 @@
+import { handler } from "./shipToElastic";
+
 describe("shipToElastic", () => {
-  it("is true", () => {
-    expect(true).toBe(true);
+  let originalValue: string | undefined;
+
+  beforeAll(() => {
+    originalValue = process.env.APP_SEARCH_ENGINE_NAME;
+    process.env.APP_SEARCH_ENGINE_NAME = "test_engine";
+  });
+
+  afterAll(() => {
+    process.env.APP_SEARCH_ENGINE_NAME = originalValue;
+  });
+
+  const client = {
+    indexDocuments: jest.fn(),
+    destroyDocuments: jest.fn(),
+  };
+
+  const shipToElastic = handler(client);
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("if a document is created, it should index it to app search", async () => {
+    const change = {
+      before: { exists: false },
+      after: {
+        id: "1",
+        exists: true,
+        data: () => {
+          return {
+            foo: "foo",
+            bar: "bar",
+          };
+        },
+      },
+    } as any;
+
+    await shipToElastic(change);
+
+    expect(client.indexDocuments).toHaveBeenCalledWith("test_engine", [
+      { id: "1", foo: "foo", bar: "bar" },
+    ]);
+  });
+
+  it("if a document is updated, it should index it to app search", async () => {
+    const change = {
+      before: {
+        id: "1",
+        exists: true,
+        data: () => {
+          return {
+            foo: "foo",
+          };
+        },
+      },
+      after: {
+        id: "1",
+        exists: true,
+        data: () => {
+          return {
+            foo: "foo",
+            bar: "bar",
+          };
+        },
+      },
+    } as any;
+
+    await shipToElastic(change);
+
+    expect(client.indexDocuments).toHaveBeenCalledWith("test_engine", [
+      { id: "1", foo: "foo", bar: "bar" },
+    ]);
+  });
+
+  it("if a document is deleted, it should deleted it from", async () => {
+    const change = {
+      before: {
+        id: "1",
+        exists: true,
+        data: () => {
+          return {
+            foo: "foo",
+            bar: "bar",
+          };
+        },
+      },
+      after: {
+        exists: false,
+      },
+    } as any;
+
+    await shipToElastic(change);
+
+    expect(client.destroyDocuments).toHaveBeenCalledWith("test_engine", ["1"]);
   });
 });
--- functions/src/shipToElastic.ts
+++ functions/src/shipToElastic.ts
@@ -4,6 +4,38 @@ import { getNewAppSearchClient } from "./utils";
 
 const appSearchClient = getNewAppSearchClient();
 
+// We separate and curry this function from shipToElastic so we can test with less mocking
+export const handler = (client: any) => {
+  return async (
+    change: functions.Change<functions.firestore.DocumentSnapshot>
+  ) => {
+    if (change.before.exists === false) {
+      // TODO Consider log levels... functions.logger.info("Hello logs!", { structuredData: true });
+      console.log(`Creating document: ${change.after.id}`);
+      client.indexDocuments(process.env.APP_SEARCH_ENGINE_NAME, [
+        {
+          id: change.after.id,
+          ...change.after.data(),
+        },
+      ]);
+    } else if (change.after.exists === false) {
+      console.log(`Deleting document: ${change.before.id}`);
+      client.destroyDocuments(process.env.APP_SEARCH_ENGINE_NAME, [
+        change.before.id,
+      ]);
+    } else {
+      console.log(`Updating document: ${change.after.id}`);
+      client.indexDocuments(process.env.APP_SEARCH_ENGINE_NAME, [
+        {
+          id: change.after.id,
+          ...change.after.data(),
+        },
+      ]);
+    }
+    return change.after;
+  };
+};
+
 // TODO handle nested fields
 // TODO handle only index specified fields: process.env.INDEXED_FIELDS
 // TODO index data with correct types? ... that depends on if we want filtering...
@@ -12,30 +44,5 @@ const appSearchClient = getNewAppSearchClient();
 // Also note that tyipcally in a function you specify the path in the call to `document` like `/${config.collectionName}/{documentId}`.
 // In an extension, the path is specified in extension.yaml, in eventTrigger.
 export const shipToElastic = functions.handler.firestore.document.onWrite(
-  async (change) => {
-    if (change.before.exists === false) {
-      // TODO Consider log levels... functions.logger.info("Hello logs!", { structuredData: true });
-      console.log(`Creating document: ${change.after.id}`);
-      appSearchClient.indexDocuments(process.env.APP_SEARCH_ENGINE_NAME, [
-        {
-          id: change.after.id,
-          ...change.after.data(),
-        },
-      ]);
-    } else if (change.after.exists === false) {
-      console.log(`Deleting document: ${change.before.id}`);
-      appSearchClient.destroyDocuments(process.env.APP_SEARCH_ENGINE_NAME, [
-        change.before.id,
-      ]);
-    } else {
-      console.log(`Updating document: ${change.after.id}`);
-      appSearchClient.indexDocuments(process.env.APP_SEARCH_ENGINE_NAME, [
-        {
-          id: change.after.id,
-          ...change.after.data(),
-        },
-      ]);
-    }
-    return change.after;
-  }
+  handler(appSearchClient)
 );
