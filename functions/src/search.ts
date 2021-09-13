ce24cc29551791105d1ce25d001d686abdafebe5
Byron Hulcher <byronhulcher@gmail.com>
2021-09-13 08:54:09 -0400

    Improve search functionality

--- functions/src/search.ts
+++ functions/src/search.ts
@@ -5,10 +5,29 @@ import { getNewAppSearchClient } from "./utils";
 const appSearchClient = getNewAppSearchClient();
 
 export const search = functions.https.onRequest(async (request, response) => {
-  const query = request.query?.query;
-  const searchResponse = await appSearchClient.search(
-    process.env.APP_SEARCH_ENGINE_NAME,
-    query || ""
+  response.set("Access-Control-Allow-Origin", "*");
+
+  functions.logger.info(
+    `Recieved search request for engine ${process.env.APP_SEARCH_ENGINE_NAME}`,
+    request.body.data
   );
-  response.send(searchResponse);
+
+  const { query, ...options } = request.body.data;
+
+  try {
+    const searchResponse = await appSearchClient.search(
+      process.env.APP_SEARCH_ENGINE_NAME,
+      query,
+      options
+    );
+
+    response.status(200).send({
+      data: searchResponse,
+    });
+  } catch (e) {
+    functions.logger.error(`Error retrieving search results`, {
+      errorMessages: e.errorMessages,
+    });
+    response.sendStatus(500);
+  }
 });
